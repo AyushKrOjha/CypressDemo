1d99b7861c7e80264398011e5acff952a4c04194
AyushKrOjha <ayush2394@outlook.com>
2022-03-14 11:56:48 +0530

    Getting Started with Cypress

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,7 +1,12 @@
+/// <reference types="Cypress" />
+
 describe('My First Test Suite', function(){
 it('My First Test Case', function(){
 
-cy.visit("https://www.google.com");
+cy.visit("https://topgeek.io/");
+cy.wait(4000);
+cy.get('.css-79wky').click();
+cy.get("div[type='Full Time']").should('have.length',7);
 
 })
 
