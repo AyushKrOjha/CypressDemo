3f2ebd6847e5c6c22338dfc3cca2105ec8c7cdb8
AyushKrOjha <ayush2394@outlook.com>
2022-03-14 18:34:35 +0530

    Asynchronous Concepts

---
+++ cypress/integration/PracticeWebsite/Test.js
@@ -0,0 +1,26 @@
+/// <reference types="Cypress" />
+
+describe('Practice Website Test Suite',function(){
+    it('First Test Cases',function(){
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+        cy.get(".search-keyword").type("ca");
+        cy.wait(4000);
+        cy.get(".product:visible").should('have.length',4);
+        //parent clild chaining
+        cy.get(".products").find(".product").should('have.length',4);
+        cy.get(".products").find(".product").eq(2).contains('ADD TO CART').click();
+
+        cy.get(".products").as('productLocator');
+        cy.get('@productLocator').find('.product').each(($el, index, $list)=>{
+
+            const textVeg = $el.find('h4.product-name').text();
+
+            if(textVeg.includes('Cashews')){
+                cy.wrap($el).find('button').click();
+            }
+        })
+        cy.get('.brand').then(function(logoElement){
+            cy.log(logoElement.text());
+        })
+    })
+})
