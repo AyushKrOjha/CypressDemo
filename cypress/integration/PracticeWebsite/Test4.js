9ea261cc57ecfe9ac9a63fa2a2888f18abfa082a
AyushKrOjha <ayush2394@outlook.com>
2022-03-15 17:47:47 +0530

    Handeling Child Browser and pop ups

---
+++ cypress/integration/PracticeWebsite/Test4.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+describe('Practice Website Alerts',function(){
+    it('Handeling Alerts',function(){
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+        cy.get('#alertbtn').click();
+        cy.get('#confirmbtn').click();
+        //window alert
+
+        cy.on('window:alert',(str)=>{
+            expect(str).to.equals('Hello , share this practice page and share your knowledge');
+        })
+
+        cy.on('window:confirm',(str)=>{
+            expect(str).to.equals('Hello , Are you sure you want to confirm?');
+        })
+
+        cy.get('#opentab').invoke('removeAttr','target').click();
+
+        cy.url().should('include','rahulshettyacademy');
+
+        cy.go('back');
+
+    })
+})
